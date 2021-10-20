a8e22352939c89aabf9bc515351447447cfc353a
UncooleBen <pengjuntao2016@foxmail.com>
2021-10-19 17:22:39 -0700

    Add a simple video handler

--- cursor_free_web_browsing/CursorFreeExtension/content.js
+++ cursor_free_web_browsing/CursorFreeExtension/content.js
@@ -1,11 +1,47 @@
 // Page specific code here
 const scriptName = "content.js: ";
+
+class VideoHandler {
+    constructor(document) {
+        this.document = document;
+        this.player = this.document.getElementsByTagName("video")[0];
+    }
+
+    mute() {
+        if (!this.player.muted) {
+            this.player.muted = true;
+            console.log(scriptName, "Muted!")
+        } else {
+            this.player.muted = false;
+            console.log(scriptName, "Unmuted!")
+        }
+    }
+
+    play() {
+        if (this.player.paused) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
+}
+
+let handler = new VideoHandler(document);
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request["sender"] === "popup") {
-        console.log(scriptName, "Sending message to background.js")
-        chrome.runtime.sendMessage(request["msgStr"], function(response) {
-            console.log(scriptName, "Background send message response: ", response);
-        });
+        if (request["action" === "sendMessage"]) {
+            console.log(scriptName, "Sending message to background.js")
+            chrome.runtime.sendMessage(request["msgStr"], function (response) {
+                console.log(scriptName, "Background send message response: ", response);
+            });
+        } else if (request["action"] === "controlVideoMute") {
+            console.log(scriptName, "Muting youtube video")
+            handler.mute();
+        } else if (request["action"] === "controlVideoPlay") {
+            console.log(scriptName, "PLaying youtube video")
+            handler.play();
+        }
     } else if (request["sender"] === "background") {
         console.log(scriptName, "Background server response: ", request["msgStr"])
     }
--- cursor_free_web_browsing/CursorFreeExtension/popup.js
+++ cursor_free_web_browsing/CursorFreeExtension/popup.js
@@ -1,9 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("button").addEventListener("click", onclick, false);
-    function onclick() {
+    document.getElementById("send_test_message").addEventListener("click", sendTestMessageOnClick, false);
+    function sendTestMessageOnClick() {
         chrome.tabs.query({ currentWindow: true, active: true },
             function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { "msgStr": "hello from chrome extension!", "sender": "popup" });
+                chrome.tabs.sendMessage(tabs[0].id, { "msgStr": "hello from chrome extension!", "sender": "popup", "action": "sendMessage" });
+            });
+    }
+
+    document.getElementById("control_video_mute").addEventListener("click", controlVideoMuteOnClick, false);
+    function controlVideoMuteOnClick() {
+        chrome.tabs.query({ currentWindow: true, active: true },
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "popup", "action": "controlVideoMute" });
+            });
+    }
+
+    document.getElementById("control_video_play").addEventListener("click", controlVideoPlayOnClick, false);
+    function controlVideoPlayOnClick() {
+        chrome.tabs.query({ currentWindow: true, active: true },
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "popup", "action": "controlVideoPlay" });
             });
     }
 
