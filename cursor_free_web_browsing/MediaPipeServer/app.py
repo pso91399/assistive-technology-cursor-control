cf21556107b8465ce33c6462b491c2efef9455d7
UncooleBen <pengjuntao2016@foxmail.com>
2021-11-04 16:47:37 -0700

    Support 4 directions

--- cursor_free_web_browsing/MediaPipeServer/app.py
+++ cursor_free_web_browsing/MediaPipeServer/app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+
 @socketio.on('message')
 def handleMessage(msg):
     print('Message:' + msg)
-    send(msg, broadcast = True)
+    send(msg, broadcast=True)
 
 
 if __name__ == '__main__':
