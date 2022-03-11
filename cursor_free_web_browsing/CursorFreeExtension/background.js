18c525394bf11115ce317e9bff8bf5dc969e8ead
Juntao Peng <juntaopeng@berkeley.edu>
2022-03-10 18:12:39 -0800

    Tuned for DEMO

--- cursor_free_web_browsing/CursorFreeExtension/background.js
+++ cursor_free_web_browsing/CursorFreeExtension/background.js
@@ -10,8 +10,24 @@ socket.on('message', function (msg) {
     console.log(scriptName, "Received message from server:", msg);
     chrome.tabs.query({ active: true, currentWindow: true },
         function (tabs) {
-            if (msg == "click") {
+            if (msg === "click") {
                 chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "click" });
+            } else if (msg == "pgup") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "pgup" });
+            } else if (msg == "pgdn") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "pgdn" });
+            } else if (msg == "forward") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "forward" });
+            } else if (msg == "backward") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "backward" });
+            } else if (msg == "play") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "play" });
+            } else if (msg == "next") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "next" });
+            } else if (msg == "mute") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "mute" });
+            } else if (msg == "close") {
+                chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "close" });
             } else {
                 chrome.tabs.sendMessage(tabs[0].id, { "sender": "server", "action": "move", "direction": msg });
             }
